bc9e1c43dc2b699705f67ecc0af964f1d96bed4f
Alexey Efimov <lex.efimov@gmail.com>
2016-04-25 16:02:54 +0500

    Реализация методов делегата

--- Logger.m
+++ Logger.m
@@ -13,4 +13,26 @@
     NSLog(@"Ouch!");
 }
 
+// Вызывается при каждом получении блока данных
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    NSLog(@"received %lu bytes", [data length]);
+    if (!incomingData) {
+        incomingData = [[NSMutableData alloc] init];
+    }
+    [incomingData appendData:data];
+}
+
+// Вызывается при обработке последнего блока
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSLog(@"Got it all!");
+    NSString *string = [[NSString alloc] initWithData:incomingData
+                                             encoding:NSUTF8StringEncoding];
+    incomingData = nil;
+    NSLog(@"The whole string is %@", string);
+}
+// Вызывается в случае ошибки при загрузке данных
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    NSLog(@"connection failed: %@", [error localizedDescription]);
+    incomingData = nil;
+}
 @end
