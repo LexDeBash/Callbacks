6e1c36dcc21aacf92c7abb8dda13a6af9f5284b7
Alexey Efimov <lex.efimov@gmail.com>
2016-04-25 14:04:14 +0500

    Создание цикла событий

--- Callbacks/main.m
+++ Callbacks/main.m
@@ -10,8 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        [[NSRunLoop currentRunLoop] run];
     }
     return 0;
 }
