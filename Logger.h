ef95d3cd8aadaf86cdf4f53fa017ce947b5d63c6
Alexey Efimov <lex.efimov@gmail.com>
2016-04-25 14:28:16 +0500

    Создание класса Logger

---
+++ Logger.h
@@ -0,0 +1,13 @@
+//
+//  Logger.h
+//  Callbacks
+//
+//  Created by Alexey Efimov on 25.04.16.
+//  Copyright © 2016 Alexey Efimov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Logger : NSObject
+- (void)sayOush:(NSTimer *)t;
+@end
---
+++ Logger.m
@@ -0,0 +1,16 @@
+//
+//  Logger.m
+//  Callbacks
+//
+//  Created by Alexey Efimov on 25.04.16.
+//  Copyright © 2016 Alexey Efimov. All rights reserved.
+//
+
+#import "Logger.h"
+
+@implementation Logger
+- (void)sayOush:(NSTimer *)t {
+    NSLog(@"Ouch!");
+}
+
+@end
